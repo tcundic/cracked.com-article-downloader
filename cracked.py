ba0561c1c5159cb907810f00ab4a71226de3b97f
Tomislav Cundić <thommyy@gmail.com>
2014-10-10 16:52:40 +0200

    Styling, new method, images sources
    In this version, i extracted part of code responsible for getting images urls from html in separate method, Added styling for centering body.

--- cracked.py
+++ cracked.py
@@ -8,11 +8,14 @@ from weasyprint import HTML
 import os
 from urllib2 import Request, urlopen, URLError, HTTPError
 import codecs
+import shutil
 
 class Cracked(object):
 	def __init__(self, url):
 		
 		self.url = url
+		self.header = None
+		self.authors = ''
 		
 		#after download put style to html
 		Cracked.__style = """
@@ -27,7 +30,7 @@ class Cracked(object):
 			img	{display:block;
 				margin-left: auto;
 				margin-right: auto;
-				margin-bottom: 30px;
+				margin-bottom: 0px;
 				margin-top: 30px;
 				}
 		</style>"""
@@ -124,46 +127,44 @@ class Cracked(object):
 						
 			self.name = self.html.find('h1').get_text()
 			
-	def __fetchImages(self, datoteka, link, mode):
+	#def __fetchImages(self, datoteka, link, mode):
+	def __fetchImages(self, datoteka, link):
 		#get image url
 		self.__url = link
-		self.__req = Request(self.__url)
-		
-		#save image locally from url
-		try:
-			self.dat = urlopen(self.__req)
-			self.lokalna_slika = open(datoteka, "w" + mode)
-			self.lokalna_slika.write((self.dat.read()))
-			self.lokalna_slika.close()
+		if(self.__url is not None):
+			self.__req = Request(self.__url)
+			
+			#save image locally from url
+			try:
+			  
+				os.system("wget -O " + datoteka + " " + link)
+				#self.dat = urlopen(self.__req)
+				#self.lokalna_slika = open(datoteka, "w" + mode)
+				#self.lokalna_slika.write((self.dat.read()))
+				#self.lokalna_slika.close()
 
-		except HTTPError, e:
-			print "HTTP Error:", e.code, self.__url
-		except URLError, e:
-			print "URL Error:", e.reason, self.__url
+			except HTTPError, e:
+				print "HTTP Error:", e.code, self.__url
+			except URLError, e:
+				print "URL Error:", e.reason, self.__url
 
-	def save_images(self):
-		#get working directory and add backslash to it
-		self.dir = os.getcwd() +'\\'
-		#in the name of the article replace spaces with _
-		self.dir += str(self.name.replace(" ", "_"))
-		#create folder in the working dir with the name of article
-		os.makedirs(self.dir)
-		
-		#change working dir to newly created folder
-		os.chdir(self.dir)
-	
+	def __saveImg(self, stranica, num):
 		#search for images in html
-		for image in self.html.findAll('img'):
-			if(image.get('src') != ""):
+		for image in stranica.find_all('img'):
+			if((image.get('data-img') is not None) and (num == 1)) or ((image.get('data-img') is not None) and (image.get('data-img') != self.header.get('data-img')) and (num == 2)):
+				#remove description of each image
+				self.desc = image.find_next_siblings('font')
+				if (len(self.desc) == 2):
+					self.desc[0].decompose()
 				#get url for each image
-				self.url = image.get('src')
+				self.url = image.get('data-img')
 				#get each image's name
-				self.imeSlike = image.get('src').rsplit('/', 1)
+				self.imeSlike = image.get('data-img').rsplit('/', 1)
 				self.imeSlike = self.imeSlike[1]
 				self.s = self.imeSlike
-				#write in html each image url as local path
-				self.dir2 = "file:///"
-				self.dir2 += self.dir
+				#write in html each image url as local path (relative)
+				#self.dir2 = "file:///"
+				self.dir2 = self.dir3
 				self.dir2 += "\\"
 				self.dir2 = self.dir2.replace("\\", "/")
 				self.dir2 += self.s
@@ -180,8 +181,9 @@ class Cracked(object):
 					self.imeSlike = self.imeSlike[0]
 					self.imeSlike += 'jpg'
 					#save image to local folder
-					self.__fetchImages(str(self.imeSlike), self.url, "b")
-				
+					#self.__fetchImages(str(self.imeSlike), self.url, "b")
+					self.__fetchImages(str(self.imeSlike), self.url)
+
 				#if image is gif format
 				elif 'gif' in self.s[1]:
 					self.s = self.s[0]
@@ -193,47 +195,97 @@ class Cracked(object):
 					self.imeSlike = self.imeSlike[0]
 					self.imeSlike += 'gif'
 					#save image to local folder
-					self.__fetchImages(str(self.imeSlike), self.url, "b")
+					#self.__fetchImages(str(self.imeSlike), self.url, "b")
+					self.__fetchImages(str(self.imeSlike), self.url)
+
+			elif((image.get('src') is not None) and (num == 1)) or ((image.get('src') is not None) and (image.get('src') != self.header.get('src')) and (num == 2)):
+				#remove description of each image
+				self.desc = image.find_next_siblings('font')
+				if (len(self.desc) == 2):
+					self.desc[0].decompose()
+				#get url for each image
+				self.url = image.get('src')
+				#get each image's name
+				self.imeSlike = image.get('src').rsplit('/', 1)
+				self.imeSlike = self.imeSlike[1]
+				self.s = self.imeSlike
+				#write in html each image url as local path (relative)
+				#self.dir2 = "file:///"
+				#self.dir2 += self.dir
+				self.dir2 = self.dir3
+				self.dir2 += "\\"
+				self.dir2 = self.dir2.replace("\\", "/")
+				self.dir2 += self.s
+				self.s = self.dir2
+				#cut extensions from image name
+				self.s = self.s.split('.')
+
+				if ('jpg' in self.s[1]) or ('jpeg' in self.s[1]):
+					self.s = self.s[0]
+					self.s += '.jpg'
+					#put local path with image name to html as tag source of image
+					image['src'] = self.s
+					#same as above, slice any text after extension
+					self.imeSlike = self.imeSlike.rsplit('jpg',1)
+					self.imeSlike = self.imeSlike[0]
+					self.imeSlike += 'jpg'
+					#save image to local folder
+					#self.__fetchImages(str(self.imeSlike), self.url, "b")
+					self.__fetchImages(str(self.imeSlike), self.url)
+
+				#if image is gif format
+				elif 'gif' in self.s[1]:
+					self.s = self.s[0]
+					self.s += '.gif'
+					#put local path with image name to html as tag source of image
+					image['src'] = self.s
+					#same as above, slice any text after extension
+					self.imeSlike = self.imeSlike.rsplit('gif',1)
+					self.imeSlike = self.imeSlike[0]
+					self.imeSlike += 'gif'
+					#save image to local folder
+					#self.__fetchImages(str(self.imeSlike), self.url, "b")
+					self.__fetchImages(str(self.imeSlike), self.url)
+		if (num == 2):
+			self.img = stranica.find('img')
+			if (self.img != None):
+				self.img.decompose()
+
+	def save_images(self):
+		#get working directory and add backslash to it
+		self.dir = os.getcwd() +'/'
+		#in the name of the article replace spaces with _
+		self.dir += str(self.name.replace(" ", "_"))
+		#create folder in the working dir with the name of article
+		os.makedirs(self.dir)
+		
+		#change working dir to newly created folder
+		os.chdir(self.dir)
+		
+		#create relative path to directory for html src images tag
+		self.dir3 = str(self.name.replace(" ", "_"))
+
+		#get header picture so it can be removed on second page
+		self.header = self.html.find('img')
+
+		#get authors of article
+		#authors
+		self.auth = self.html.find(class_=re.compile("meta"))		
+		#each author is element with byline class
+		for author in self.auth.findAll(class_=re.compile("byline")):
+			#if string authors don't have apostrophe add opening
+			if(self.authors.find('"') == -1):
+				self.authors += '"'
+			#concate each author following with comma
+			self.authors += author.get_text()
+			#if that's last author, add closing apostrophe
+			if(author.get_text().find(',') == -1):
+				self.authors += '"'
+
+		self.__saveImg(self.html, 1)
 
 		if (self.page2):
-			for image in self.html2.findAll('img'):
-				if(image.get('src') != ""):
-					self.url = image.get('src')
-					#get each image's name
-					self.imeSlike = image.get('src').rsplit('/', 1)
-					self.imeSlike = self.imeSlike[1]
-					self.s = self.imeSlike
-					#write in html each image url as local path
-					self.dir2 = "file:///"
-					self.dir2 += self.dir
-					self.dir2 += "\\"
-					self.dir2 = self.dir2.replace("\\", "/")
-					self.dir2 += self.s
-					self.s = self.dir2
-					self.s = self.s.split('.')
-					if ('jpg' in self.s[1]) or ('jpeg' in self.s[1]):
-						self.s = self.s[0]
-						self.s += '.jpg'
-						#put local path with image name to html as tag source of image
-						image['src'] = self.s
-						#same as above, slice any text after extension
-						self.imeSlike = self.imeSlike.rsplit('jpg',1)
-						self.imeSlike = self.imeSlike[0]
-						self.imeSlike += 'jpg'
-						#save image to local folder
-						self.__fetchImages(str(self.imeSlike), self.url, "b")
-
-					elif 'gif' in self.s[1]:
-						self.s = self.s[0]
-						self.s += '.gif'
-						#put local path with image name to html as tag source of image
-						image['src'] = self.s
-						#same as above, slice any text after extension
-						self.imeSlike = self.imeSlike.rsplit('gif',1)
-						self.imeSlike = self.imeSlike[0]
-						self.imeSlike += 'gif'
-						#save image to local folder
-						self.__fetchImages(str(self.imeSlike), self.url, "b")
+			self.__saveImg(self.html2, 2)
 
 	def save_WebPage(self):
 		#return up one folder
@@ -259,9 +311,14 @@ class Cracked(object):
 	
 	def convertToEpub(self):
 		#command line arguments
-		self.convertArg = '"' + self.name + '"' + ' ' + '"' + self.epub + '"' + ' --embed-font-family calibri --toc-threshold 50 --max-toc-links 0 --level1-toc //h:h1 --level2-toc //h:h2 --no-default-epub-cover --language English'
+		#self.convertArg = '"' + self.name + '"' + ' ' + '"' + self.epub + '"' + ' --toc-threshold 50 --max-toc-links 0 --level1-toc //h:h1 --level2-toc //h:h2 --no-default-epub-cover --language English --authors ' + self.authors + ' --extra-css stylesheet.css'
+		self.convertArg = '"' + self.name + '"' + ' ' + '"' + self.epub + '"' + ' --toc-threshold 50 --max-toc-links 0 --level1-toc //h:h1 --level2-toc //h:h2 --no-default-epub-cover --language English --authors ' + self.authors
 		#run ebook converter with arguments in command line
-		os.system("ebook-convert.exe" + " " + self.convertArg)
+		os.system("ebook-convert" + " " + self.convertArg.encode('utf8'))
+		#remove html
+		os.remove(self.name)
+		#remove folder with article images
+		shutil.rmtree(self.dir)
 
 #if you don't send argument article url on script running you can paste it after from command line
 if(len(sys.argv) < 2):
