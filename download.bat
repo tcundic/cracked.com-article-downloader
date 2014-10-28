58048483f1feccc5e6e1f90c8e483c008f23c5a2
Tomislav Cundić <tcundic@gmail.com>
2014-10-28 15:52:37 +0100

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,13 +1,6 @@
-REM download.bat 1> log.dat 2>&1
-python.exe cracked.py http://www.cracked.com/blog/5-horrible-things-you-learn-working-at-convenience-store/
-python.exe cracked.py http://www.cracked.com/blog/7-needlessly-difficult-features-every-retro-video-game/
-python.exe cracked.py http://www.cracked.com/blog/5-ways-borderlands-2-remorseless-addiction-machine/
-python.exe cracked.py http://www.cracked.com/article_20673_5-video-games-with-disturbing-implications-you-didnt-notice.html
-python.exe cracked.py http://www.cracked.com/article_20639_the-6-most-horrifying-ways-routine-surgeries-have-gone-wrong.html
-python.exe cracked.py http://www.cracked.com/blog/the-spookiest-ghost-story-ever-told/
-python.exe cracked.py http://www.cracked.com/blog/5-things-to-remember-when-your-life-goes-to-hell/
-python.exe cracked.py http://www.cracked.com/blog/the-5-most-baffling-christmas-related-crimes-ever/
-python.exe cracked.py http://www.cracked.com/blog/5-things-in-life-youre-never-really-prepared-for/
-python.exe cracked.py http://www.cracked.com/blog/5-reminders-we-could-all-use-right-now/
-python.exe cracked.py http://www.cracked.com/blog/5-things-men-will-never-understand-about-consoling-woman/
-python.exe cracked.py http://www.cracked.com/blog/5-common-reactions-that-make-you-regret-good-deed/
+REM in windows start download.bat from command prompt this way
+REM C:\download.bat 1> log.dat 2>&1
+REM it write all output to cmd and save it in file "log.dat"
+python.exe cracked.py REM http://www.cracked.com/url-of-article 1....
+python.exe cracked.py REM http://www.cracked.com/url-of-article 2....
+python.exe cracked.py REM http://www.cracked.com/url-of-article 3....
